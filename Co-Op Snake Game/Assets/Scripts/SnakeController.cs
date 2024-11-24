131a3c4733df1ff86169488c6b1026d603b5846f
Aakash Singh <aaka241@gmail.com>
2024-11-24 06:09:01 +0530

    added screen wrapping and tail

--- Co-Op Snake Game/Assets/Scripts/SnakeController.cs
+++ Co-Op Snake Game/Assets/Scripts/SnakeController.cs
@@ -8,6 +8,17 @@ namespace SnakeGame
     {
         private Vector2 dirction = Vector2.right;
 
+        private List<Transform> snakeTail;
+        public Transform snakeTailPrefab;
+
+        public BoxCollider2D SpawnArea;
+
+        private void Start()
+        {
+            snakeTail = new List<Transform>();  
+            snakeTail.Add(transform);
+        }
+
         private void Update()
         {
             if (Input.GetKeyDown(KeyCode.W)) { dirction = Vector2.up; }
@@ -18,14 +29,63 @@ namespace SnakeGame
 
         private void FixedUpdate()
         { 
+            for(int i = snakeTail.Count - 1; i > 0; i--)
+            {
+                snakeTail[i].position = snakeTail[i - 1].position;
+            }
+
             this.transform.position = new Vector3
             (
                 Mathf.Round(this.transform.position.x + dirction.x),
                 Mathf.Round(this.transform.position.y + dirction.y),
                 0.0f
             );
-            
+
+            WrapSnakeInBounds();
+
         }
 
+        protected void WrapSnakeInBounds()
+        {
+            Bounds bounds = SpawnArea.bounds;
+
+            Vector3 snakeHeadPosition = this.transform.position;
+
+            if (snakeHeadPosition.x > bounds.max.x)
+            {
+                snakeHeadPosition.x = bounds.min.x;
+            }
+            else if (snakeHeadPosition.x < bounds.min.x)
+            {
+                snakeHeadPosition.x = bounds.max.x;
+            }
+
+            if (snakeHeadPosition.y > bounds.max.y)
+            {
+                snakeHeadPosition.y = bounds.min.y;
+            }
+            else if (snakeHeadPosition.y < bounds.min.y)
+            {
+                snakeHeadPosition.y = bounds.max.y;
+            }
+
+            this.transform.position = snakeHeadPosition;
+        }
+
+        private void Grow()
+        {
+            Transform tail = Instantiate(this.snakeTailPrefab);
+            tail.position = snakeTail[snakeTail.Count - 1].position; 
+
+            snakeTail.Add(tail);   
+        }
+
+         private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.tag == "MassGainer")
+            {
+                Grow();
+            }
+        }
     }
 }
